fd4b1ab6ab90a6b9065292fa3d1355dea25114ce
psugne <ugne.piscikaite@gmail.com>
2024-11-04 18:41:33 +0200

    [MIG] web_action_conditionable

--- web_action_conditionable/__manifest__.py
+++ web_action_conditionable/__manifest__.py
@@ -1,8 +1,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "web_action_conditionable",
-    "version": "16.0.1.0.0",
-    "depends": ["base", "web"],
+    "version": "17.0.1.0.0",
+    "depends": ["web"],
     "data": [],
     "author": "Cristian Salamea,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
--- web_action_conditionable/static/src/components/field_one2many.esm.js
+++ web_action_conditionable/static/src/components/field_one2many.esm.js
@@ -1,33 +1,31 @@
 /** @odoo-module **/
-import {X2ManyField} from "@web/views/fields/x2many/x2many_field";
-import {XMLParser} from "@web/core/utils/xml";
-import {evaluateExpr} from "@web/core/py_js/py";
-import {patch} from "@web/core/utils/patch";
+import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
+import { XMLParser } from "@web/core/utils/xml";
+import { evaluateExpr } from "@web/core/py_js/py";
+import { patch } from "@web/core/utils/patch";
 
-patch(X2ManyField.prototype, "web_action_conditionable_FieldOne2Many", {
+patch(X2ManyField.prototype, {
     get rendererProps() {
         this.updateActiveActions();
-        return this._super(...arguments);
+        return super.rendererProps;  // Use 'super' directly in Odoo 17
     },
     updateActiveActions() {
         if (this.viewMode === "list" && this.activeActions.type === "one2many") {
-            const self = this;
             const parser = new XMLParser();
             const archInfo = this.activeField.views[this.viewMode];
             const xmlDoc = parser.parseXML(archInfo.__rawArch);
-            ["create", "delete"].forEach(function (item) {
-                if (self.activeActions[item] && _.has(xmlDoc.attributes, item)) {
+
+            ["create", "delete"].forEach((item) => {
+                if (this.activeActions[item] && xmlDoc.attributes?.[item]) {
                     const expr = xmlDoc.getAttribute(item);
                     try {
-                        self.activeActions[item] = evaluateExpr(
+                        this.activeActions[item] = evaluateExpr(
                             expr,
-                            self.props.record.data
+                            this.props.record.data
                         );
                     } catch (ignored) {
-                        console.log(
-                            "[web_action_conditionable] unrecognized expr '" +
-                                expr +
-                                "', ignoring"
+                        console.warn(
+                            `[web_action_conditionable] Unrecognized expr '${expr}', ignoring`
                         );
                     }
                 }
